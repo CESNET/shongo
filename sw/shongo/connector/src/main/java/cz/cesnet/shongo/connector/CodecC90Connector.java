ba3cf8b111e9f0760a881d3bd6149da1af463ec2
Ondrej Bouda <ondrej.bouda@cesnet.cz>
2012-07-16 11:28:09 +0200

    CodecC90 connector - simplified command passing to the device through SSH

--- sw/shongo/connector/src/main/java/cz/cesnet/shongo/connector/CodecC90Connector.java
+++ sw/shongo/connector/src/main/java/cz/cesnet/shongo/connector/CodecC90Connector.java
@@ -4,19 +4,18 @@ import com.jcraft.jsch.ChannelShell;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import cz.cesnet.shongo.connector.api.ConnectorInfo;
-import cz.cesnet.shongo.connector.api.EndpointService;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * A connector for Cisco TelePresence System Codec C90.
  *
  * @author Ondrej Bouda <ondrej.bouda@cesnet.cz>
  */
-public class CodecC90Connector implements EndpointService
+public class CodecC90Connector //implements EndpointService // FIXME: implement the EndpointService interface
 {
     public static void main(String[] args) throws IOException, JSchException, InterruptedException
     {
@@ -53,28 +52,17 @@ public class CodecC90Connector implements EndpointService
         final CodecC90Connector conn = new CodecC90Connector();
         conn.connect(address, username, password);
         conn.exec("xstatus SystemUnit uptime");
+        Thread.sleep(4000); // FIXME: when exec blocks, remove sleeping
         conn.disconnect();
     }
 
 
     /**
-     * A stream for providing commands to the device.
-     */
-    private CommandInputStream commandInputStream = new CommandInputStream();
-
-    /**
-     * A reader for results of commands.
-     */
-    private CommandResultStream commandResultStream = new CommandResultStream(new String[]{
-            "\r\nOK\r\n",
-            "\r\nERROR\r\n",
-            "</XmlDoc>",
-    });
-
-    /**
      * Shell channel open to the device.
      */
     private ChannelShell channel;
+
+    private OutputStreamWriter commandStreamWriter;
 
 
     public void connect(String address, String username, final String password)
@@ -93,8 +81,7 @@ public class CodecC90Connector implements EndpointService
         session.setConfig("StrictHostKeyChecking", "no");
         session.connect();
         channel = (ChannelShell) session.openChannel("shell");
-        channel.setInputStream(commandInputStream);
-//        channel.setOutputStream(commandResultStream);
+        commandStreamWriter = new OutputStreamWriter(channel.getOutputStream());
         channel.setOutputStream(System.out);
         channel.connect(); // runs a separate thread for handling the streams
 
@@ -128,9 +115,10 @@ public class CodecC90Connector implements EndpointService
      */
     private String exec(String command, boolean expectOutput) throws IOException, InterruptedException
     {
-        commandInputStream.pushCommand(command);
+        commandStreamWriter.write(command + '\n');
+        commandStreamWriter.flush();
         if (expectOutput) {
-            return commandResultStream.getOutput();
+            return null; // FIXME: block and return the complete command output
         }
         else {
             return null;
@@ -140,52 +128,6 @@ public class CodecC90Connector implements EndpointService
     private String exec(String command) throws IOException, InterruptedException
     {
         return exec(command, false);
-    }
-
-    @Override
-    public void dial(String server)
-    {
-    }
-
-    @Override
-    public void resetDevice()
-    {
-    }
-
-    @Override
-    public void mute()
-    {
-    }
-
-    @Override
-    public void unmute()
-    {
-    }
-
-    @Override
-    public void setMicrophoneLevel(int level)
-    {
-    }
-
-    @Override
-    public void setPlaybackLevel(int level)
-    {
-    }
-
-    @Override
-    public void enableVideo()
-    {
-    }
-
-    @Override
-    public void disableVideo()
-    {
-    }
-
-    @Override
-    public ConnectorInfo getConnectorInfo()
-    {
-        return null;
     }
 }
 
