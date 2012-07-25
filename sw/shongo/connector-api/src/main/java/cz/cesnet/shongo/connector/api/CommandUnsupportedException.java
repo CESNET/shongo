6f7a5aa04b62af0d9d1dfbe5bb6828fea7135913
Ondrej Bouda <ondrej.bouda@cesnet.cz>
2012-07-25 19:45:21 +0200

    implemented the Codec C90 connector

---
+++ sw/shongo/connector-api/src/main/java/cz/cesnet/shongo/connector/api/CommandUnsupportedException.java
@@ -0,0 +1,19 @@
+package cz.cesnet.shongo.connector.api;
+
+/**
+ * An exception thrown when a command is not supported on a specific device.
+ *
+ * @author Ondrej Bouda <ondrej.bouda@cesnet.cz>
+ */
+public class CommandUnsupportedException extends Exception
+{
+    public CommandUnsupportedException(String message)
+    {
+        super(message);
+    }
+
+    public CommandUnsupportedException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
--- sw/shongo/connector-api/src/main/java/cz/cesnet/shongo/connector/api/EndpointService.java
+++ sw/shongo/connector-api/src/main/java/cz/cesnet/shongo/connector/api/EndpointService.java
@@ -11,43 +11,53 @@ public interface EndpointService extends CommonService
      * Dials a server.
      * @param server    server address to dial
      */
-    void dial(Alias server) throws CommandException;
+    void dial(Alias server) throws CommandException, CommandUnsupportedException;
 
     /**
      * Resets the device.
      */
-    void resetDevice() throws CommandException;
+    void resetDevice() throws CommandException, CommandUnsupportedException;
 
     /**
      * Mutes this endpoint.
      */
-    void mute() throws CommandException;
+    void mute() throws CommandException, CommandUnsupportedException;
 
     /**
      * Unmutes this endpoint.
      */
-    void unmute() throws CommandException;
+    void unmute() throws CommandException, CommandUnsupportedException;
 
     /**
-     * Sets microphone audio level of this endpoint to a given value.
+     * Sets microphone (all microphones) audio level of this endpoint to a given value.
      * @param level         microphone level to set
      */
-    void setMicrophoneLevel(int level) throws CommandException;
+    void setMicrophoneLevel(int level) throws CommandException, CommandUnsupportedException;
 
     /**
      * Sets playback audio level of this endpoint to a given value.
      * @param level         microphone level to set
      */
-    void setPlaybackLevel(int level) throws CommandException;
+    void setPlaybackLevel(int level) throws CommandException, CommandUnsupportedException;
 
     /**
      * Enables video from this endpoint.
      */
-    void enableVideo() throws CommandException;
+    void enableVideo() throws CommandException, CommandUnsupportedException;
 
     /**
      * Disables video from this endpoint.
      */
-    void disableVideo() throws CommandException;
+    void disableVideo() throws CommandException, CommandUnsupportedException;
+
+    /**
+     * Starts the presentation mode (turns on the media stream).
+     */
+    void startPresentation() throws CommandException, CommandUnsupportedException;
+
+    /**
+     * Stop the presentation mode (turns off the media stream).
+     */
+    void stopPresentation() throws CommandException, CommandUnsupportedException;
 
 }
