0310680098fcd884bbc0f11aba236e39de171a75
Martin Srom <martin.srom@mail.muni.cz>
2012-11-20 08:21:56 +0100

    Fixed some TODOs.

--- sw/shongo/controller/src/main/java/cz/cesnet/shongo/controller/executor/EndpointProvider.java
+++ sw/shongo/controller/src/main/java/cz/cesnet/shongo/controller/executor/EndpointProvider.java
@@ -1,7 +1,11 @@
 package cz.cesnet.shongo.controller.executor;
 
+import cz.cesnet.shongo.controller.request.Specification;
+import cz.cesnet.shongo.controller.reservation.Reservation;
+
 /**
- * TODO:
+ * Interface which can be implemented by {@link Specification}s or by {@link Reservation}s and the object
+ * which implements it provides an {@link Endpoint} (which then can be used e.g., in {@link Compartment}).
  *
  * @author Martin Srom <martin.srom@cesnet.cz>
  */
