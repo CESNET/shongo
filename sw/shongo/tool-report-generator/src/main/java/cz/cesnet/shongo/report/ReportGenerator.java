e841b9e7dd1d5c191bd03bc9f2e213e06c9da415
Martin Srom <martin.srom@mail.muni.cz>
2013-03-12 14:34:05 +0100

    Added tool for report generating.

---
+++ sw/shongo/tool-report-generator/src/main/java/cz/cesnet/shongo/report/ReportGenerator.java
@@ -0,0 +1,14 @@
+package cz.cesnet.shongo.report;
+
+/**
+ * TODO:
+ *
+ * @author Martin Srom <martin.srom@cesnet.cz>
+ */
+public class ReportGenerator
+{
+    public static void main(String[] args)
+    {
+        throw new RuntimeException("TODO: implement");
+    }
+}
