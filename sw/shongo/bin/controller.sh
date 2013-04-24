d847df1e57d03e5d4d782bade351981035758dc1
Martin Srom <martin.srom@mail.muni.cz>
2013-04-24 16:09:10 +0200

    Added script for dumping reservation requests.

--- sw/shongo/bin/controller.sh
+++ sw/shongo/bin/controller.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
 cd `dirname $0`/../
 VERSION=`cat pom.xml | grep '<shongo.version>' | sed -e 's/.\+>\(.\+\)<.\+/\1/g'`
-java -jar controller/target/controller-$VERSION.jar "$@"
+
+case $1 in
+    reservation-requests)
+        shift
+        for id in $(bin/client-cli.sh src --connect $1 --root --scripting --cmd "list-reservation-requests" | jsawk -n "out(this.id)"); do
+            bin/client-cli.sh src --connect $1 --root --scripting --cmd "get-reservation-request $id"
+        done;
+
+        ;;
+    *)
+        java -jar controller/target/controller-$VERSION.jar "$@"
+        ;;
+esac
