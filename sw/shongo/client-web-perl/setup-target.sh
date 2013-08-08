bfd71c944b873c918da1337b9b3e77e264e6ef1e
Martin Srom <martin.srom@mail.muni.cz>
2013-08-08 13:25:53 +0200

    Deleted old web client in Perl and the custom maven plugin for executing shell commands.

--- sw/shongo/client-web-perl/setup-target.sh
+++
@@ -1,9 +0,0 @@
-# Go to this folder
-cd `dirname $0`
-
-# Create log and setup permissions for it
-mkdir -p ../log
-chmod go+w ../log
-
-# setup permissions for entry point
-chmod a+x target/www/index.pl
