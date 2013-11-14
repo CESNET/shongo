35be0fda3c563f05c8077a3dc2f23e1a5bf78571
Martin Srom <martin.srom@mail.muni.cz>
2013-11-14 07:15:04 +0100

    [client-web] Do not report EofExceptions (just log them as warnings).

--- sw/shongo/client-web/src/main/java/cz/cesnet/shongo/client/web/ClientWebHandlerExceptionResolver.java
+++ sw/shongo/client-web/src/main/java/cz/cesnet/shongo/client/web/ClientWebHandlerExceptionResolver.java
@@ -5,8 +5,11 @@ import cz.cesnet.shongo.client.web.models.ErrorModel;
 import cz.cesnet.shongo.controller.ControllerConnectException;
 import cz.cesnet.shongo.controller.api.rpc.CommonService;
 import net.tanesha.recaptcha.ReCaptcha;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +24,8 @@ import java.util.Map;
  */
 public class ClientWebHandlerExceptionResolver implements HandlerExceptionResolver
 {
+    private static Logger logger = LoggerFactory.getLogger(ClientWebHandlerExceptionResolver.class);
+
     @Resource
     private ClientWebConfiguration configuration;
 
@@ -37,6 +42,11 @@ public class ClientWebHandlerExceptionResolver implements HandlerExceptionResolv
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         if (exception instanceof ControllerConnectException) {
             return new ModelAndView("controllerNotAvailable");
+        }
+        else if (exception instanceof org.eclipse.jetty.io.EofException) {
+            // Just log that exceptions and do not report it
+            logger.warn("Not reported exception.", exception);
+            return null;
         }
         ErrorModel errorModel = new ErrorModel(request.getRequestURI(), null, null, exception, request);
         ModelAndView modelAndView = ErrorController.handleError(errorModel, configuration, reCaptcha, commonService);
--- sw/shongo/client-web/src/main/java/cz/cesnet/shongo/client/web/controllers/IndexController.java
+++ sw/shongo/client-web/src/main/java/cz/cesnet/shongo/client/web/controllers/IndexController.java
@@ -1,28 +1,13 @@
 package cz.cesnet.shongo.client.web.controllers;
 
-import cz.cesnet.shongo.TodoImplementException;
 import cz.cesnet.shongo.client.web.ClientWebUrl;
-import cz.cesnet.shongo.client.web.models.*;
-import cz.cesnet.shongo.controller.api.ExecutableSummary;
-import cz.cesnet.shongo.controller.api.SecurityToken;
-import cz.cesnet.shongo.controller.api.request.ExecutableListRequest;
-import cz.cesnet.shongo.controller.api.request.ListResponse;
-import cz.cesnet.shongo.controller.api.rpc.ExecutableService;
-import cz.cesnet.shongo.util.DateTimeFormatter;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Interval;
-import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.*;
 
 /**
  * Index controller.
