9266f717e2c738c39b1466556a9696e64dd97acc
Martin Srom <martin.srom@mail.muni.cz>
2014-01-02 09:05:12 +0100

    [db] Added migration script for storing recording folders in permanent rooms and not in used rooms.

--- sw/shongo/migrations/2013-12-18/migration.sql
+++ sw/shongo/migrations/2013-12-18/migration.sql
@@ -1,7 +1,22 @@
 BEGIN TRANSACTION;
 
-todo: create resource_room_endpoint_recording_folder_ids
-todo: move room_endpoint_recording_folder_ids to resource_room_endpoint_recording_folder_ids
-todo: drop room_endpoint_recording_folder_ids
+/* Create resource_room_endpoint_recording_folder_ids */
+CREATE TABLE resource_room_endpoint_recording_folder_ids (resource_room_endpoint_id int8 NOT NULL, recording_folder_id VARCHAR(255), recording_capability_id int8 NOT NULL, PRIMARY KEY (resource_room_endpoint_id, recording_capability_id));
+ALTER TABLE resource_room_endpoint_recording_folder_ids ADD CONSTRAINT FKCF0984C1815AF60 FOREIGN KEY (recording_capability_id) REFERENCES recording_capability;
+ALTER TABLE resource_room_endpoint_recording_folder_ids ADD CONSTRAINT FKCF0984CA35D7203 FOREIGN KEY (resource_room_endpoint_id) REFERENCES resource_room_endpoint;
+
+/* Copy room_endpoint_recording_folder_ids to resource_room_endpoint_recording_folder_ids */
+INSERT INTO resource_room_endpoint_recording_folder_ids(resource_room_endpoint_id, recording_folder_id , recording_capability_id)
+    SELECT
+        COALESCE(used_room_endpoint.room_endpoint_id, room_endpoint.id),
+        room_endpoint_recording_folder_ids.recording_folder_id,
+        room_endpoint_recording_folder_ids.recording_capability_id
+    FROM room_endpoint
+    LEFT JOIN used_room_endpoint ON used_room_endpoint.id = room_endpoint.id
+    LEFT JOIN room_endpoint_recording_folder_ids ON room_endpoint_recording_folder_ids.room_endpoint_id = room_endpoint.id
+    WHERE room_endpoint_recording_folder_ids.recording_folder_id IS NOT NULL;
+
+/* Drop room_endpoint_recording_folder_ids */
+DROP TABLE room_endpoint_recording_folder_ids;
 
 COMMIT TRANSACTION;
