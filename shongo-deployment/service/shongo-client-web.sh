d276cba921970449a8986afc9d9953729f4095e6
Martin Srom <martin.srom@cesnet.cz>
2014-07-22 13:51:46 +0200

    [bin] Fixed encoding parameter in scripts.

--- shongo-deployment/service/shongo-client-web.sh
+++ shongo-deployment/service/shongo-client-web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=shongo-client-web
-BIN="java -jar ../shongo-client-web/target/shongo-client-web-:VERSION:.jar --daemon -Dfile.encoding=UTF-8"
+BIN="java -Dfile.encoding=UTF-8 -jar ../shongo-client-web/target/shongo-client-web-:VERSION:.jar --daemon"
 BIN_STARTED="ClientWeb successfully started"
 
 source $(dirname $0)/shongo-service.sh
--- shongo-deployment/service/shongo-connector.sh
+++ shongo-deployment/service/shongo-connector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=shongo-connector
-BIN="java -jar ../shongo-connector/target/shongo-connector-:VERSION:.jar --daemon -Dfile.encoding=UTF-8"
+BIN="java -Dfile.encoding=UTF-8 -jar ../shongo-connector/target/shongo-connector-:VERSION:.jar --daemon"
 BIN_STARTED="Connector successfully started"
 
 source $(dirname $0)/shongo-service.sh
