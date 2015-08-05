683389c72c8c6a1cdb2b70baf79b37bf45c20f41
Ondrej Pavelka <pavelka@cesnet.cz>
2015-08-05 15:23:34 +0200

    [controller] fixed reservation_list.sql

--- shongo-controller/src/main/resources/sql/reservation_list.sql
+++ shongo-controller/src/main/resources/sql/reservation_list.sql
@@ -11,6 +11,7 @@ SELECT
     reservation_summary.slot_start AS slot_start,
     reservation_summary.slot_end AS slot_end,
     reservation_summary.resource_id AS resource_id,
+    reservation_summary.foreign_resources_id as foreign_resources_id,
     reservation_summary.room_license_count AS room_license_count,
     reservation_summary.room_name AS room_name,
     reservation_summary.alias_types AS alias_types,
