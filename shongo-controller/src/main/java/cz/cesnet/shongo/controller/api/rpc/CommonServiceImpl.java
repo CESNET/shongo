c58fa0fce48b0ac80082992416752c1e699bee0e
Ondrej Pavelka <pavelka@cesnet.cz>
2015-09-17 17:58:57 +0200

    [controller] EntityManager check

--- shongo-controller/src/main/java/cz/cesnet/shongo/controller/api/rpc/CommonServiceImpl.java
+++ shongo-controller/src/main/java/cz/cesnet/shongo/controller/api/rpc/CommonServiceImpl.java
@@ -107,51 +107,54 @@ public class CommonServiceImpl extends AbstractServiceImpl
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         ResourceManager resourceManager = new ResourceManager(entityManager);
 
-        List<DeviceResource> deviceResourceList = resourceManager.listManagedDevices();
-        Map<String, DeviceResource> deviceResourceMap = new HashMap<String, DeviceResource>();
-        for (DeviceResource deviceResource : deviceResourceList) {
-            String agentName = ((cz.cesnet.shongo.controller.booking.resource.ManagedMode) deviceResource.getMode())
-                    .getConnectorAgentName();
-            deviceResourceMap.put(agentName, deviceResource);
-        }
+        try {
+            List<DeviceResource> deviceResourceList = resourceManager.listManagedDevices();
+            Map<String, DeviceResource> deviceResourceMap = new HashMap<String, DeviceResource>();
+            for (DeviceResource deviceResource : deviceResourceList) {
+                String agentName = ((cz.cesnet.shongo.controller.booking.resource.ManagedMode) deviceResource.getMode())
+                        .getConnectorAgentName();
+                deviceResourceMap.put(agentName, deviceResource);
+            }
 
-        List<Connector> connectorList = new ArrayList<Connector>();
-        for (AID aid : controllerAgent.listConnectorAgents()) {
-            String agentName = aid.getLocalName();
+            List<Connector> connectorList = new ArrayList<Connector>();
+            for (AID aid : controllerAgent.listConnectorAgents()) {
+                String agentName = aid.getLocalName();
 
-            Connector connector = new Connector();
-            connector.setName(agentName);
+                Connector connector = new Connector();
+                connector.setName(agentName);
 
-            SendLocalCommand sendLocalCommand = controllerAgent.sendCommand(agentName, new GetStatus());
-            if (sendLocalCommand.getState().equals(SendLocalCommand.State.SUCCESSFUL)) {
-                ConnectorStatus connectorStatus = (ConnectorStatus) sendLocalCommand.getResult();
-                connector.setAgentState(Connector.AgentState.AVAILABLE);
-                connector.setStatus(connectorStatus);
-            } else {
+                SendLocalCommand sendLocalCommand = controllerAgent.sendCommand(agentName, new GetStatus());
+                if (sendLocalCommand.getState().equals(SendLocalCommand.State.SUCCESSFUL)) {
+                    ConnectorStatus connectorStatus = (ConnectorStatus) sendLocalCommand.getResult();
+                    connector.setAgentState(Connector.AgentState.AVAILABLE);
+                    connector.setStatus(connectorStatus);
+                } else {
+                    connector.setAgentState(Connector.AgentState.NOT_AVAILABLE);
+                    connector.setStatus(new ConnectorStatus(ConnectorStatus.State.NOT_AVAILABLE));
+                }
+
+                DeviceResource deviceResource = deviceResourceMap.get(agentName);
+                if (deviceResource != null) {
+                    connector.setResourceId(ObjectIdentifier.formatId(deviceResource));
+                    deviceResourceMap.remove(agentName);
+                }
+
+                connectorList.add(connector);
+            }
+
+            for (Map.Entry<String, DeviceResource> entry : deviceResourceMap.entrySet()) {
+                Connector connector = new Connector();
+                connector.setName(entry.getKey());
+                connector.setResourceId(ObjectIdentifier.formatId(entry.getValue()));
                 connector.setAgentState(Connector.AgentState.NOT_AVAILABLE);
                 connector.setStatus(new ConnectorStatus(ConnectorStatus.State.NOT_AVAILABLE));
+                connectorList.add(connector);
             }
 
-            DeviceResource deviceResource = deviceResourceMap.get(agentName);
-            if (deviceResource != null) {
-                connector.setResourceId(ObjectIdentifier.formatId(deviceResource));
-                deviceResourceMap.remove(agentName);
-            }
-
-            connectorList.add(connector);
+            return connectorList;
         }
-
-        for (Map.Entry<String, DeviceResource> entry : deviceResourceMap.entrySet()) {
-            Connector connector = new Connector();
-            connector.setName(entry.getKey());
-            connector.setResourceId(ObjectIdentifier.formatId(entry.getValue()));
-            connector.setAgentState(Connector.AgentState.NOT_AVAILABLE);
-            connector.setStatus(new ConnectorStatus(ConnectorStatus.State.NOT_AVAILABLE));
-            connectorList.add(connector);
+        finally {
+            entityManager.close();
         }
-
-        entityManager.close();
-
-        return connectorList;
     }
 }
