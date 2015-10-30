fd47a65e4962874d3eeed5fa70b795f7d98608ad
Ondrej Pavelka <pavelka@cesnet.cz>
2015-10-30 17:39:51 +0100

    [controller] concurrent bug fix v1

--- shongo-controller/src/main/java/cz/cesnet/shongo/controller/booking/room/RoomReservationTask.java
+++ shongo-controller/src/main/java/cz/cesnet/shongo/controller/booking/room/RoomReservationTask.java
@@ -905,7 +905,7 @@ public class RoomReservationTask extends ReservationTask
      * @param capabilities to filter
      * @param userId which should contain {@link Domain}'s id
      */
-    private void filterCapabilitiesByUser(Collection<RoomProviderCapability> capabilities, String userId)
+    private synchronized void filterCapabilitiesByUser(Collection<RoomProviderCapability> capabilities, String userId)
     {
         if (!schedulerContext.isLocalByUser() && capabilities != null && !capabilities.isEmpty() && Controller.isInterDomainInitialized()) {
             cz.cesnet.shongo.controller.api.Domain domain = new cz.cesnet.shongo.controller.api.Domain();
