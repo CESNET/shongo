593848c872ae331b57a8e873299ce97efc6edc65
Ondrej Pavelka <pavelka@cesnet.cz>
2016-09-01 08:51:44 +0200

    [deployment] fix of migration sql

--- shongo-deployment/migrations/2016-08-22/migration.sql
+++ shongo-deployment/migrations/2016-08-22/migration.sql
@@ -3,9 +3,6 @@
  */
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS executable_summary;
-DROP TABLE IF EXISTS specification_summary;
-
 DROP VIEW IF EXISTS executable_summary;
 DROP VIEW IF EXISTS specification_summary;
 
