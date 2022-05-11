c763af4b18734ea08011d39600bc509890b489fa
Filip Karnis <karnisfilip@gmail.com>
2022-05-12 01:25:09 +0200

    Changed edit request http method from put to patch

--- shongo-controller/src/main/java/cz/cesnet/shongo/controller/rest/api/ReservationRequestController.java
+++ shongo-controller/src/main/java/cz/cesnet/shongo/controller/rest/api/ReservationRequestController.java
@@ -27,9 +27,9 @@ import org.joda.time.Interval;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -254,7 +254,7 @@ public class ReservationRequestController {
     }
 
     @Operation(summary = "Modifies reservation request.")
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     void modifyRequest(
             @RequestAttribute(TOKEN) SecurityToken securityToken,
             @PathVariable("id") String id,
